b1dd6e8af5bbd041aa3a6b819b1a64dc4e28ee5a
Mark Lilback <mark@lilback.com>
2018-01-18 00:07:18 -0500

    added func to log a start entry to all handlers

--- Sources/MJLLogger/Logger.swift
+++ Sources/MJLLogger/Logger.swift
@@ -10,7 +10,7 @@ import Foundation
 public final class Logger {
 	let configuration: LogConfiguration
 	internal var handlers = [LogHandler]()
-	
+	private var started: Bool = false
 	
 	public init(config: LogConfiguration = DefaultLogConfiguration())
 	{
@@ -19,6 +19,22 @@ public final class Logger {
 	
 	public func append(handler: LogHandler) {
 		handlers.append(handler)
+		if started {
+			handler.append(entry: LogEntry(type: .start))
+		}
+	}
+	
+	public func remove(handler: LogHandler) {
+		guard let idx = handlers.index(where: { handler.equals($0) }) else { return }
+		handlers.remove(at: idx)
+	}
+	
+	/// Logs a .start LogEntry to all handlers. Any handlers added later will have a start message logged
+	public func logApplicationStart() {
+		assert(!started, "already started")
+		started = true
+		let startEntry = LogEntry(type: .start)
+		handlers.forEach { $0.append(entry: startEntry)}
 	}
 	
 	public func log(_ entry: LogEntry)
