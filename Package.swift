d1cb8dfca502ff73ec86cfb86c7d37291208b914
Mark Lilback <mark@lilback.com>
2020-10-06 17:34:08 -0400

    updated swift-log dependency

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["MJLLogger"]),
     ],
     dependencies: [
-		.package(url: "https://github.com/apple/swift-log.git", from: "1.1.1"),
+		.package(url: "https://github.com/apple/swift-log.git", from: "1.4.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
